7a81390cf89369004b020f633f621cece142e4ac
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-04-25 14:03:09 +0200

    added dependancies - added scripts - generated client

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import { PostsApi, VotesApi, MediaApi } from "./posts_client/api";
+declare const _default: {
+    PostsApi: typeof PostsApi;
+    VotesApi: typeof VotesApi;
+    MediaApi: typeof MediaApi;
+};
+export default _default;
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var api_1 = require("./posts_client/api");
+exports["default"] = { PostsApi: api_1.PostsApi, VotesApi: api_1.VotesApi, MediaApi: api_1.MediaApi };
